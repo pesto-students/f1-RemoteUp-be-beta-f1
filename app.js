7a64158b7fbc36f6c6d3058569b804a9e09c5d58
Minith Jain <minithsatawat@hotmail.com>
2021-10-20 12:24:22 +0530

    Fix CORS issue

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const con = require('./utils/databaseConfig');
+var cors = require("cors");
 // const { httpLogger } = require('./utils/logger');
 
 // Databse connection
@@ -10,6 +11,7 @@ con.on('open', () => {
 
 // Express setup
 const app = express();
+app.use(cors());
 app.use(express.json());
 // app.use(httpLogger);
 
