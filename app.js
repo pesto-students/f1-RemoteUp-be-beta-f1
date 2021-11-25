60cb2cb2807cd142954beb7b8f8a663ba39f6659
bhupen13au <mbhupendrads@gmail.com>
2021-11-25 19:19:49 +0530

    Fix: limit of json data and Fix: cron-job for every midnight

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ con.on('open', () => {
 // Express setup
 const app = express();
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
 
 // cors middleware
 app.use((req, res, next) => {
@@ -38,5 +38,5 @@ app.listen(PORT, () => {
 });
 
 // cron-job to mark expired job as inactive at everyday midnight
-// cron.schedule('0 0 0 * * *', () => { taskJobExpiry(); });
-cron.schedule('*/60 * * * *', () => { taskJobExpiry(); });
+cron.schedule('0 0 0 * * *', () => { taskJobExpiry(); });
+// cron.schedule('*/60 * * * *', () => { taskJobExpiry(); });
